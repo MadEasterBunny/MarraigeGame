a0e65b12e2ca42821ff84c1ac9f13a0238698312
Matthew Huntington Murphy <huntington67@aim.com>
2019-06-07 00:55:37 +0900

    結婚式ゲームのエネミー移動とプレイヤーの攻撃を追加しました。 ・基本的なエネミー移動とプレイヤーがエネミーを攻撃すると、エネミーが後ろに少し移動します。 ※エネミーの後ろに移動が変に動いているので、調整する必要があります。

---
+++ Assets/Scripts/KnockBack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public float thrust;
+    public float knockTime;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = collision.GetComponent<Rigidbody2D>();
+            if(enemy != null)
+            {
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+            }
+        }
+    }
+
+    IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if(enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) >= attackRadius)
+        {
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            myRigidbody.MovePosition(temp);
+        }
+    }
+}
