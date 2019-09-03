4f35ad77dd6972a2c401b21b36b81cde677e528d
Matthew Huntington Murphy <huntington67@aim.com>
2019-09-04 01:12:26 +0900

    マシューのストーリーシーン作成
    マシュー分の話が理解やすくなるため、大学に戻るきっかけに関するダイアログシーンを作成しビデオに追加しました。

---
+++ Assets/Scripts/Dialogue/FadingText.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadingText : MonoBehaviour
+{
+    [Header("Text and Variables")]
+    public Text fadingText;
+    public Text subtitlesText;
+    public GameObject subtitles;
+    [TextArea(2, 5)]
+    public string[] storyText;
+    [TextArea(2, 5)]
+    public string[] subtitleText;
+    private int currentTextIndex;
+    public bool advanceStory;
+
+    [Space]
+    [Header("Fade Variables")]
+    public float fadingTime;
+    public float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTextIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(FadeText());
+    }
+
+    public void AdvanceText()
+    {
+        if(currentTextIndex <= storyText.Length)
+        {
+            currentTextIndex++;
+        }
+    }
+
+    public IEnumerator FadeText()
+    {
+        while(currentTextIndex <= storyText.Length)
+        {
+            fadingText.canvasRenderer.SetAlpha(0.0f);
+            fadingText.text = storyText[currentTextIndex];
+            subtitlesText.text = subtitleText[currentTextIndex];
+            fadingText.CrossFadeAlpha(1, fadingTime, false);
+            yield return new WaitForSeconds(waitTime);
+            subtitles.SetActive(true);
+            fadingText.CrossFadeAlpha(0, fadingTime, false);
+            yield return new WaitForSeconds(waitTime);
+            fadingText.canvasRenderer.SetAlpha(0.0f);
+            AdvanceText();
+            if (currentTextIndex >= storyText.Length)
+            {
+                currentTextIndex = 0;
+                fadingText.canvasRenderer.SetAlpha(0.0f);
+                //yield return new WaitForSeconds(0.2f);
+                subtitles.SetActive(false);
+                advanceStory = true;
+                yield break;
+            }
+        } 
+    }
+}
---
+++ Assets/Scripts/Objects/Transitions/StoryTransition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryTransition : MonoBehaviour
+{
+    [Header("Confirmation Object")]
+    public GameObject advStory;
+
+    [Space]
+    [Header("Load Time")]
+    public float waitTime;
+
+    [Space]
+    [Header("Scene to Load")]
+    public string sceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    public IEnumerator LoadScene()
+    {
+        if (advStory.GetComponent<FadingText>().advanceStory == true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            /*AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+            while (!async.isDone)
+            {
+                yield return null;
+            }*/
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
