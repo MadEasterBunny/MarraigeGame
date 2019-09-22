df22a9ed336e0630f730631af01ef1bcd3e8e7f4
Matthew Huntington Murphy <huntington67@aim.com>
2019-09-22 16:06:21 +0900

    イギリス、早紀シーン、空港シーンまでコンテンツ追加しました。
    イギリスで留学するシーン(バス効果音要り)、留学したあとのシーンから空港のシーンまでコンテンツを追加しました。

---
+++ Assets/Scripts/Other/EnableFlowchart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableFlowchart : MonoBehaviour
+{
+    public GameObject flowChart;
+    public GameObject sceneChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            flowChart.SetActive(true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            sceneChange.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Other/MoveLeft.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
