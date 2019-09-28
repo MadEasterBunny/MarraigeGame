4c0ec75239086a2d7c428e9a8fa070793bcfecdc
Matthew Huntington Murphy <huntington67@aim.com>
2019-09-28 18:59:59 +0900

    全体の修正が完了しました。
    全体のプロジェクトが完了し、今から動画を撮影します。

---
+++ Assets/Scripts/Controller/AnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Saki'sHouseInterior2")
+        {
+            anim.SetBool("isDrinking", true);
+        }
+        else
+        {
+            anim.SetBool("isDrinking", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Transitions/PlaceText.cs
+++ Assets/Scripts/Transitions/PlaceText.cs
@@ -13,6 +13,19 @@ public class PlaceText : MonoBehaviour
     public GameObject subtitlesTextbox;
     public Text subtitlesText;
 
+    public float waitTime;
+    public float waitTimeAlpha;
+    public float waitTimeOpaque;
+
+    private void Awake()
+    {
+        if (placeText.CompareTag("Ending"))
+        {
+            placeText.canvasRenderer.SetAlpha(0.0f);
+            //placeText.CrossFadeAlpha(0, 0, false);
+        }
+    }
+
     private void OnEnable()
     {
         StartCoroutine(ShowPlaceText());
@@ -21,14 +34,30 @@ public class PlaceText : MonoBehaviour
 
     public IEnumerator ShowPlaceText()
     {
-        text.SetActive(true);
-        placeText.text = placeName;
-        placeText.CrossFadeAlpha(1, 2, false);
-        yield return new WaitForSeconds(2f);
-        placeText.CrossFadeAlpha(0, 2, false);
-        yield return new WaitForSeconds(2f);
-        text.SetActive(false);
-        placeText.canvasRenderer.SetAlpha(0.0f);
+        if(placeText.CompareTag("Ending"))
+        {
+            text.SetActive(true);
+            placeText.text = placeName;
+            yield return new WaitForSeconds(waitTimeAlpha);
+            placeText.CrossFadeAlpha(1, waitTime, false);
+            yield return new WaitForSeconds(waitTimeOpaque);
+            placeText.CrossFadeAlpha(0, waitTime, false);
+            yield return new WaitForSeconds(waitTime);
+            text.SetActive(false);
+            placeText.canvasRenderer.SetAlpha(0.0f);
+        }
+        else
+        {
+            text.SetActive(true);
+            placeText.text = placeName;
+            placeText.CrossFadeAlpha(1, 2, false);
+            yield return new WaitForSeconds(2f);
+            placeText.CrossFadeAlpha(0, 2, false);
+            yield return new WaitForSeconds(2f);
+            text.SetActive(false);
+            placeText.canvasRenderer.SetAlpha(0.0f);
+        }
+
     }
 
     public IEnumerator Subtitles()
